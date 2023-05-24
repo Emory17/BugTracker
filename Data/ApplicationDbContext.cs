aa80c7f05d8d3eb83d03a5cef20044a049dd7ce6
Emory Soper <emorysoper@icloud.com>
2023-05-24 17:58:24 -0400

    Scaffolding

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using BugTracker.Models;
 
 namespace BugTracker.Data
 {
@@ -9,5 +10,14 @@ namespace BugTracker.Data
             : base(options)
         {
         }
+        public DbSet<BugTracker.Models.Company> Company { get; set; } = default!;
+        public DbSet<BugTracker.Models.Invite> Invite { get; set; } = default!;
+        public DbSet<BugTracker.Models.Notification> Notification { get; set; } = default!;
+        public DbSet<BugTracker.Models.NotificationType> NotificationType { get; set; } = default!;
+        public DbSet<BugTracker.Models.Project> Project { get; set; } = default!;
+        public DbSet<BugTracker.Models.Ticket> Ticket { get; set; } = default!;
+        public DbSet<BugTracker.Models.TicketAttachment> TicketAttachment { get; set; } = default!;
+        public DbSet<BugTracker.Models.TicketComment> TicketComment { get; set; } = default!;
+        public DbSet<BugTracker.Models.TicketHistory> TicketHistory { get; set; } = default!;
     }
 }
