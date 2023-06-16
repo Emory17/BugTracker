f7ff735df4d1ff3f3929e0d0fd81656820527aa9
Emory Soper <emorysoper@icloud.com>
2023-06-16 17:59:06 -0400

    Login

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace BugTracker.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Dashboard()
         {
             return View();
         }
